f4b5895a3c6e8b968616104cd67afbaf3c850f51
Gustavo Moneiro <dinizmonteirogustavo@gmail.com>
2020-06-15 00:04:42 -0300

    Add support to define development database.

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
 FLASK_ENV=development
 FLASK_APP=app.py
+FLASK_DB_URI=sqlite+pysqlite:///database.db
--- bookmark/models/base.py
+++ bookmark/models/base.py
@@ -3,7 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+def get_uri():
+    if os.getenv('FLASK_ENV') == 'development':
+        return os.getenv('FLASK_DB_URI')
+    return os.getenv('DB_URI')
 
-engine = create_engine(os.getenv('DB_URI'))
+engine = create_engine(get_uri())
 Session = sessionmaker(engine)
 Model = declarative_base()
